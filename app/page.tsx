be9be5463518e280aed3e153522b1bc9113c3652
Asjadnasim <asjadnasim@gmail.com>
2024-02-02 18:18:27 +0530

    Completed Hero Section and i stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Hero from '../components/Hero';
+import Info from '@/components/Info';
+import Hero from '@/components/Hero';
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
+			<Info />
 		</>
 	);
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,11 +6,41 @@ import 'swiper/css/autoplay';
 import 'swiper/css/pagination';
 
 import { Autoplay, Pagination } from 'swiper/modules';
+import Image from 'next/image';
 
 const Hero = () => {
 	return (
 		<div className='mt-2'>
-			<div className='main-container'></div>
+			<div className='main-container'>
+				<Swiper
+					modules={[Autoplay, Pagination]}
+					autoplay={{ delay: 5000 }}
+					speed={500}
+					pagination={{ clickable: true }}
+					spaceBetween={10}
+					slidesPerView={1}
+					className='h-full w-full'
+				>
+					<SwiperSlide>
+						<img
+							src={'/slider-image-1.png'}
+							alt='Slider img - 1 '
+							// width={500}
+							// height={500}
+							className='h-full w-full object-cover'
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img
+							src={'/slider-image-2.png'}
+							alt='Slider Image - 1 '
+							// width={500}
+							// height={500}
+							className='h-full w-full object-cover'
+						/>
+					</SwiperSlide>
+				</Swiper>
+			</div>
 		</div>
 	);
 };
